26798fea6c21f20bb3ac2254c2b60b3b24c4d4f5
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-25 09:44:26 +0300

    smile images

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Form from './components/Form/Form.jsx';
 import Btn from './components/Btn/Btn.jsx';
 import Map from './components/Map/Map.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import { ReactComponent as SmileOrangee } from "./images/smiles/1.svg";
+import { ReactComponent as SmileOrange } from "./images/smiles/2.svg";
+import { ReactComponent as Smile } from "./images/smiles/3.svg";
 
 function App() {
   return (
     <Container>
+      <SmileOrangee width="88.32px" height="85.84px" />
       <Title title='Reach out to us!' />
       <Form />
-      <Btn />
+      <Btn btntitle='Send message' />
       <Map />
-      <Footer btntitle='Send message' />
+      <SmileOrange width="88.32px" height="85.84px" />
+      <Smile width="214px" height="208px" />
+      <Footer />
     </Container>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { ReactComponent as Smile } from "../../images/smiles/3.svg";
 import { ReactComponent as In } from "../../images/socials/in.svg";
 import { ReactComponent as Facebook } from "../../images/socials/facebook.svg";
 import { ReactComponent as Twitter } from "../../images/socials/tw.svg";
 import { ReactComponent as Addition } from "../../images/socials/q.svg";
+import { ReactComponent as SmileReverse } from "../../images/smiles/4.svg";
+import { ReactComponent as SmileOrangee } from "../../images/smiles/5.svg";
 
 const Footer = () => {
   return (
     <div>
+      <Smile width="214px" height="208px" />
       <In />
       <Facebook />
       <Twitter />
       <Addition />
+      <SmileReverse width="78.46px" height="97px" />
+      <SmileOrangee width="88.32px" height="85.84px" />
     </div>
   );
 };
