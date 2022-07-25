09d9565713f6f47d8f647c9fe34094f4a1922e31
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-25 14:34:10 +0300

    fix position of smiles

--- src/App.js
+++ src/App.js
@@ -37,15 +37,21 @@ function App() {
   // `;
 
   return (
-    <Container>
-      <SmileOrangee width="88.32px" height="85.84px" style={{ marginLeft: '29px', marginTop: '23px' }} />
-      <div style={{ marginLeft: '150px' }}>
-        <Title>Reach out to us!</Title>
-        {/* <Title title='Reach out to us!' /> */}
-        <Form />
-        {/* <Btn btntitle='Send message' /> */}
-      </div>
-      {/* <Map>
+    <div
+      style={{
+        position: 'relative',
+        overflow: 'hidden',
+      }}
+    >
+      <Container>
+        <SmileOrangee width="88.32px" height="85.84px" style={{ marginLeft: '29px', marginTop: '23px' }} />
+        <div style={{ marginLeft: '150px' }}>
+          <Title>Reach out to us!</Title>
+          {/* <Title title='Reach out to us!' /> */}
+          <Form />
+          {/* <Btn btntitle='Send message' /> */}
+        </div>
+        {/* <Map>
         <iframe
           title="map"
           ssrc="https://www.google.com/maps/place/East+New+York,+%D0%91%D1%80%D1%83%D0%BA%D0%BB%D1%96%D0%BD,+%D0%9D%D1%8C%D1%8E-%D0%99%D0%BE%D1%80%D0%BA,+%D0%A1%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D1%96+%D0%A8%D1%82%D0%B0%D1%82%D0%B8+%D0%90%D0%BC%D0%B5%D1%80%D0%B8%D0%BA%D0%B8/@40.6958789,-74.0172129,10.83z/data=!4m5!3m4!1s0x89c25d065dd654f3:0xea460699be6d9dd4!8m2!3d40.6590529!4d-73.8759245"
@@ -53,33 +59,34 @@ function App() {
           loading="lazy"
         ></iframe>
       </Map> */}
-      <MapImage width="976px" height="976px" style={{
-        position: "absolute",
-        top: "0%",
-        left: "50%"
-      }} />
-      <Smile width="214px" height="208px" style={{
-        position: "absolute",
-        top: "105%",
-        left: "60%"
-      }} />
-      <SmileOrange width="88.32px" height="85.84px" style={{
-        position: "absolute",
-        top: "100%",
-        left: "60%"
-      }} />
-      <Footer />
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover />
-    </Container>
+        <MapImage width="976px" height="976px" style={{
+          position: "absolute",
+          top: "0%",
+          left: "50%"
+        }} />
+        <Smile width="214px" height="208px" style={{
+          position: "absolute",
+          top: "105%",
+          left: "60%"
+        }} />
+        <SmileOrange width="88.32px" height="85.84px" style={{
+          position: "absolute",
+          top: "100%",
+          left: "60%"
+        }} />
+        <Footer />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover />
+      </Container>
+    </div >
   );
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,74 +1,85 @@
-import React from "react";
-import { ReactComponent as Smile } from "../../images/smiles/3.svg";
-import { ReactComponent as In } from "../../images/socials/in.svg";
-import { ReactComponent as Facebook } from "../../images/socials/facebook.svg";
-import { ReactComponent as Twitter } from "../../images/socials/tw.svg";
-import { ReactComponent as Addition } from "../../images/socials/q.svg";
-import { ReactComponent as SmileReverse } from "../../images/smiles/4.svg";
-import { ReactComponent as SmileOrangee } from "../../images/smiles/5.svg";
+import React from 'react';
+import { ReactComponent as Smile } from '../../images/smiles/3.svg';
+import { ReactComponent as In } from '../../images/socials/in.svg';
+import { ReactComponent as Facebook } from '../../images/socials/facebook.svg';
+import { ReactComponent as Twitter } from '../../images/socials/tw.svg';
+import { ReactComponent as Addition } from '../../images/socials/q.svg';
+import { ReactComponent as SmileReverse } from '../../images/smiles/4.svg';
+import { ReactComponent as SmileOrangee } from '../../images/smiles/5.svg';
+import s from './Footer.module.css';
 
 const Footer = () => {
   return (
     <div
       style={{
-        marginTop: "100px",
-        display: "flex",
-        height: "200px",
-        background: "#FAFAFA",
-        border: "1px solid #D8D8D8",
+        position: 'relative',
+        marginTop: '100px',
+        display: 'flex',
+        height: '200px',
+        background: '#FAFAFA',
+        border: '1px solid #D8D8D8',
+        overflow: 'hidden',
       }}
     >
       <Smile
         width="300px"
         height="300px"
         style={{
-          position: "absolute",
-          top: "195%",
+          position: 'absolute',
+          top: '-20%',
         }}
       />
       <div
         style={{
-          marginRight: "50px",
-          marginTop: "79px",
-          marginLeft: "340px",
+          marginRight: '50px',
+          marginTop: '60px',
+          marginLeft: '340px',
         }}
       >
-        <In
-          style={{
-            marginLeft: "25px",
-          }}
-        />
-        <Facebook
-          style={{
-            marginLeft: "25px",
-          }}
-        />
-        <Twitter
-          style={{
-            marginLeft: "25px",
-          }}
-        />
-        <Addition
-          style={{
-            marginLeft: "25px",
-          }}
-        />
+        <a href="https://www.linkedin.com/" className={s.link}>
+          <In
+            style={{
+              marginLeft: '25px',
+            }}
+          />
+        </a>
+        <a href="https://www.facebook.com/" className={s.link}>
+          <Facebook
+            style={{
+              marginLeft: '25px',
+            }}
+          />
+        </a>
+        <a href="https://www.twitter.com/" className={s.link}>
+          <Twitter
+            style={{
+              marginLeft: '25px',
+            }}
+          />
+        </a>
+        <a href="https://www.google.com/" className={s.link}>
+          <Addition
+            style={{
+              marginLeft: '25px',
+            }}
+          />
+        </a>
       </div>
       <SmileReverse
         width="78.46px"
         height="97px"
         style={{
-          marginRight: "128px",
-          marginLeft: "auto",
+          marginRight: '128px',
+          marginLeft: 'auto',
         }}
       />
       <SmileOrangee
         width="88.32px"
         height="85.84px"
         style={{
-          position: "absolute",
-          left: "94%",
-          top: "210%",
+          position: 'absolute',
+          left: '94%',
+          top: '20%',
         }}
       />
     </div>
