d7f411b3c754cc9e18113bb07016ad5ac5a13278
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-25 09:26:27 +0300

    ReactComponent Svg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { ReactComponent as In } from "../../images/socials/in.svg";
+import { ReactComponent as Facebook } from "../../images/socials/facebook.svg";
+import { ReactComponent as Twitter } from "../../images/socials/tw.svg";
+import { ReactComponent as Addition } from "../../images/socials/q.svg";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <In />
+      <Facebook />
+      <Twitter />
+      <Addition />
+    </div>
+  );
 };
 
 export default Footer;
