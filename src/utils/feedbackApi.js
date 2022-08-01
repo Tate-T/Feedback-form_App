39259fcfa89c1c6e0800cd972977d61fa48efdfe
Tate-T <tatjana.tarasovych@gmail.com>
2022-08-01 14:23:10 +0300

    mockapi feedbackData

--- src/utils/feedbackApi.js
+++ src/utils/feedbackApi.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
 
-axios.defaults.baseURL = 'https://';
+axios.defaults.baseURL = 'https://6216b55b71e7672e53694d33.mockapi.io/';
 
-export const addNewFeedbackApi = feedback => {
-    return axios.post('/feedback', feedback).then(({ data }) => {
-        console.log(data.newFeedback);
+export const addNewFeedbackApi = newFeedback => {
+    return axios.post('/feedback', newFeedback).then(({ data }) => {
+
         return (
-            Notify.success("Ваш відгук відправлено"))
-        // {
-        // title: data.newFeedback.title,
-        // author: data.newFeedback.author,
-        // publishYear: data.newFeedback.publishYear,
-        // pagesTotal: data.newFeedback.pagesTotal,
-        // pagesFinished: data.newFeedback.pagesFinished, // or 0 as initial value for a new book in the library
-        // _id: data.newFeedback._id,
-        // };
+            Notify.success("Ваш відгук відправлено")) &
+        {
+            name: data.newFeedback.name,
+            email: data.newFeedback.email,
+            message: data.newFeedback.message,
+            id: data.newFeedback.id,
+        };
     });
 };
